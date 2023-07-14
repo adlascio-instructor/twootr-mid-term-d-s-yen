cbf98dd736721aebd0d2a1da397ba843caaa723f
unknown <skd.itkn@gmail.com>
2023-07-14 15:54:42 -0700

    foundation of main section

---
+++ frontend/components/posts.jsx
@@ -0,0 +1,12 @@
+import Post from "./post.jsx";
+
+export default function Posts(){
+    return (
+        
+        <section className="posts">
+            Posts arae
+            <Post/>
+        </section>
+        
+    )
+}
