bcd649f9d9c443896b35bf24b27ca1467e7aa1b4
YenLun <yenlunliu@gmail.com>
2023-07-15 22:38:39 -0700

    article area rendering done

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -4,28 +4,33 @@ import Posts from "./Posts.jsx";
 
 import Styled from "styled-components";
 import axios from "axios";
+import { useState, useEffect } from "react";
 
-axios.get(`http://localhost:8080/twoots`)
-.then((data) => {
-    console.log("Twoots:", data.data);
-})
-.catch(err => {
-    console.log("ERROR:", err);
-});
-
-const Main1= Styled.main`
-    padding: 0 100px;
-    margin: 10px auto;
+const Main1 = Styled.main`
+padding: 0 100px;
+margin: 10px auto;
 `;
 
-export default function Main(){
-    return (
+export default function Main() {
+    const [postdata, setPostdata] = useState([]);
 
+    useEffect(() => {
+        axios
+            .get(`http://localhost:8080/twoots`)
+            .then((data) => {
+                console.log("Twoots:", data.data);
+                setPostdata([...data.data]);
+            })
+            .catch((err) => {
+                console.log("ERROR:", err);
+            });
+    }, []);
+
+    return (
         <Main1>
             <Account />
             <Form />
-            <Posts />
+            <Posts postdata={postdata} />
         </Main1>
-
-    )
+    );
 }
