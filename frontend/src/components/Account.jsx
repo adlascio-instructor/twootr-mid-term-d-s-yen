1f26366478a8d835ca881bce2c817238764a7530
YenLun <yenlunliu@gmail.com>
2023-07-15 23:27:26 -0700

    adding btn in Account

--- frontend/src/components/Account.jsx
+++ frontend/src/components/Account.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 const Section = styled.section`
@@ -19,15 +20,23 @@ const Icon = styled.img`
 `;
 
 export default function Account() {
+    const [click, setClick] = useState(0);
+
+    const handleClick = () => {
+        setClick(console.log("ya, u've clicked!"));
+    };
+
     return (
         <Section>
             <img
                 src="https://avatars.dicebear.com/api/bottts/${authorSlug}.svg"
                 height={"150px"}
             />
+
             <Div>
                 <h3>John Doe</h3>
                 <Icon
+                    onClick={handleClick}
                     src="https://icons.veryicon.com/png/o/miscellaneous/geometry-icon-library/input-21.png"
                     height={"30rem"}
                 />
