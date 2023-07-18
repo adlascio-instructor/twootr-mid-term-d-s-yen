72efd2a30b5af56b88c3ea51866c38baf0753723
unknown <skd.itkn@gmail.com>
2023-07-17 21:22:50 -0700

    another commit

--- frontend/src/components/Icons.jsx
+++ frontend/src/components/Icons.jsx
@@ -10,10 +10,10 @@ export default function Icons(){
     const bgColor=(e)=>{
         if(e.target.classList.contains("on")){
             e.target.style.fill="#555555"
-            e.target.classList.remove("On")
+            e.target.classList.remove("on")
         }else{
             e.target.style.fill="rgb(255,55,180)"
-            e.target.classList.add("On")
+            e.target.classList.add("on")
         }
     }
 
