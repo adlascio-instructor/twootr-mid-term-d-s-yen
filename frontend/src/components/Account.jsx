32f48537df05bca314d6cc634177d09f7841a5e3
YenLun <yenlunliu@gmail.com>
2023-07-19 09:03:28 -0700

    adding checked icon

--- frontend/src/components/Account.jsx
+++ frontend/src/components/Account.jsx
@@ -27,11 +27,28 @@ const Icon = styled.img`
     margin-left: 0.5rem;
 `;
 
+const CheckedIcon = styled.img`
+    margin-left: 0.5rem;
+    height: 35px;
+`;
+
+const UserAccordingInput = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+`;
+
 export default function Account() {
     const [input, setInput] = useState("noShow");
-    const [name, setName] = useState("noShow");
+    const [name, setName] = useState("show");
+    const [icon, setIcon] = useState("show");
+    const [checkedIcon, setCheckedIcon] = useState("noShow");
+
     const isShowingInput = input === "noShow";
-    const isShowingName = name === "noShow";
+    const isShowingName = name === "show";
+    const isShowingCheckedIcon = checkedIcon === "noShow";
+    const isShowingIcon = icon === "show";
 
     const [inputFirstValue, setInputFirstValue] = useState("John");
     const [inputSecondValue, setInputSecondValue] = useState("Doe");
@@ -45,8 +62,10 @@ export default function Account() {
     };
 
     const toggle = () => {
-        setName(!"noShowshow");
+        setName(!"show");
         setInput(!"show");
+        setCheckedIcon(!"show");
+        setIcon(!"show");
     };
 
     return (
@@ -70,15 +89,20 @@ export default function Account() {
                     onChange={handleSecondValue}
                 />
                 <Icon
+                    style={isShowingIcon ? showing : notShowing}
                     onClick={toggle}
                     src="https://icons.veryicon.com/png/o/miscellaneous/geometry-icon-library/input-21.png"
                     height={"30rem"}
-                >
-                    {/* {state ? "close" : "open"} */}
-                </Icon>
+                />
+                <CheckedIcon
+                    style={isShowingCheckedIcon ? notShowing : showing}
+                    src="https://cdn-icons-png.flaticon.com/512/5289/5289675.png"
+                />
             </Div>
-            <p>{inputFirstValue}</p>
-            <p>{inputSecondValue}</p>
+            <UserAccordingInput>
+                <h4>{inputFirstValue}</h4>
+                <h4>{inputSecondValue}</h4>
+            </UserAccordingInput>
         </Section>
     );
 }
