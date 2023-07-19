0769800d9815a4d1d7fdd20970b5aff4ee85f5d3
YenLun <yenlunliu@gmail.com>
2023-07-19 12:04:55 -0700

    add down arrow btn

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -10,16 +10,30 @@ const FirstH3 = styled.h3`
     margin-left: 1rem;
 `;
 
-const SecondH3 = styled.h3`
+const RightDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     margin-left: auto;
     margin-right: 1rem;
+`;
+
+const IMG = styled.img`
+    justify-content: center;
+    align-items: center;
 `;
 
 export default function Header() {
     return (
         <Div>
             <FirstH3>TWOOTR</FirstH3>
-            <SecondH3>Write a new tweet</SecondH3>
+            <RightDiv>
+                <h3>Write a new tweet</h3>
+                <IMG
+                    src="https://cdn-icons-png.flaticon.com/256/9053/9053268.png"
+                    height="30px"
+                />
+            </RightDiv>
         </Div>
     );
 }
